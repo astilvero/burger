2381b53b089c38168eb3677fe17e01632cf3e284
astilvero <astilvero6@gmail.com>
2021-12-15 08:25:28 -0500

    Start

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import Auxi from '../../hoc/Auxi';
+import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+    state= {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () =>{
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler = () =>{
+        this.setState((prevState) => {
+            return {showSidedrawer: !prevState.showSideDrawer};
+        });
+    }
+
+    render() {
+    return(
+        <Auxi>
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+            <SideDrawer 
+                open={this.state.showSideDrawer} 
+                closed={this.sideDrawerClosedHandler}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Auxi>
+        )
+    }
+} 
+
+export default Layout;
---
+++ src/hoc/Auxi.js
@@ -0,0 +1,4 @@
+//elemento de raiz envolvente
+const auxi = (props) => props.children;
+
+export default auxi;
